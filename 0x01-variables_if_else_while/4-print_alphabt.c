c5795847b5d27884c611b8b8a8854a05aca22022
alex-m-kimeu <alexkimeu1999@gmail.com>
2022-09-09 12:25:13 -0700

    program that prints the alphabet in lowercase except q and e, followed by a new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -Entry point
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
