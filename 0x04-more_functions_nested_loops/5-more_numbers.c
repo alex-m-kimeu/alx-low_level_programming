79f9490b2ad9386a83ec480efd1d650507217a7d
Alex Kimeu <alexkimeu1999@gmail.com>
2022-09-15 14:11:37 +0300

    Correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,13 @@ void more_numbers(void)
 {
 	int i = 0, j;
 
-	while (i < 10)
+	while (i <= 9)
 	{
 		j = 0;
 
 		while (j <= 14)
 		{
+			if (j > 9)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			j++;
