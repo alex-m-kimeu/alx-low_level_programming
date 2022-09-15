6a8f4463e9eb8dd85a08b31d8df6d25a4e4913b3
Alex Kimeu <alexkimeu1999@gmail.com>
2022-09-15 15:31:11 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - entry point
+* prints "Fizz" for numbers divisible by 3,
+* prints "Buzz" for numbers divisible by 5,
+* prints "FizzBuzz" for numbers divisible by both 3 and 5
+* numbers 1 to 0
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("%s ", f);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+                        printf("%s ", fb);
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
