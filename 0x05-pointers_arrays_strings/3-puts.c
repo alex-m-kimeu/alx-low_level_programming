b3af1ac283dff57eae82b8469fb8f64c16eab9e5
Alex Kimeu <alexkimeu1999@gmail.com>
2022-09-19 21:33:22 +0300

    prints a string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,11 +8,12 @@
 
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i])
 	{
-		_putchar(str[i]);
+		_putchar(str[count]);
+		i++;
 	}
 
 	_putchar('\n');
