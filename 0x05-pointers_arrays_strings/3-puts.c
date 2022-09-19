cfad508dcba4792a5fbaa8bb0bcd81d47e08915f
Alex Kimeu <alexkimeu1999@gmail.com>
2022-09-19 21:20:01 +0300

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: String to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i))
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+
+	_putchar('\n');
+}
